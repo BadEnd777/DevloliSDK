282ee653a93e7c7572d82baf9edb060ca02dede4
BadEnd <badend23@hotmail.com>
2024-01-27 11:45:03 +0700

    Update BugReport.cs

--- DevloliSDK/Discord/DevloliWebhook/BugReport.cs
+++ DevloliSDK/Discord/DevloliWebhook/BugReport.cs
@@ -4,7 +4,7 @@ using UnityEngine.Networking;
 
 public class BugReport : EditorWindow
 {
-    string webhookURL = "https://discord.com/api/webhooks/960897876274323466/682oA45d7ZZHM5DbpDG0jc3Yp-SbuD5QNr_UgNqo12hAIlrGK4hHY7Z_0XXe65N87CNN";
+    string webhookURL = "";
     string message = "";
     string username = "DevloliSDK";
     string avatar = "https://devloli-main.github.io/images/Dllbg.png";
